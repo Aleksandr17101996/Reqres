e274bc282174348bb79229d32afab44a1c50652f
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-05-03 15:04:08 +0300

    add base_page

---
+++ WEB/pages/base_page.py
@@ -0,0 +1,18 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    def go_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
