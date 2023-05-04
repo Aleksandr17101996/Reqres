2107848139ca17a4414f1d2832ba82e3eb78cbb2
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-05-04 11:06:03 +0300

    add test click get single resource

--- WEB/tests/main_test.py
+++ WEB/tests/main_test.py
@@ -20,7 +20,7 @@ class TestRequests:
 
         def test_click_get_single_user(self, driver):
             """Проверяем что при нажатии на отправку запроса о полчении информации о пользователе,
-               возвращается статус кода 200 и отображается информация о 6 пользователях """
+               возвращается статус кода 200, в теле ответа есть информация о пользователе """
 
             page = Requests(driver, 'https://reqres.in/')
             page.open()
@@ -30,4 +30,36 @@ class TestRequests:
             assert status_code == 200, "Статус кода не соответсвует ожидаемому"
             assert "data" in body
 
+        def test_click_get_single_user_not_fo(self, driver):
+            """Проверяем что при нажатии на отправку запроса о полчении информации о пользователе,
+               через несуществующий id, возвращается статус кода 404"""
 
+            page = Requests(driver, 'https://reqres.in/')
+            page.open()
+            page.click_get_single_user_not_found()
+            status_code = page.check_status_code_bud()
+            assert status_code == 404, "Статус кода несоответсвует ожидаемому"
+
+        def test_click_get_list_resource(self, driver):
+            """Проверяем что при нажатии на отправку запроса о полчении информации о пользователях,
+               возвращается статус кода 200 и отображается информация о 6 пользователях """
+
+            page = Requests(driver, 'https://reqres.in/')
+            page.open()
+            page.click_get_list_resource()
+            status_code = page.check_status_code()
+            body = page.check_result()
+            assert status_code == 200, "Статус кода не соответсвует"
+            assert len(body['data']) == 6, "Не верное колличество пользователей на странице"
+
+        def test_click_get_single_resource(self, driver):
+            """Проверяем что при нажатии на отправку запроса о полчении информации о пользователе,
+               возвращается статус кода 200, в теле ответа есть информация о пользователе """
+
+            page = Requests(driver, 'https://reqres.in/')
+            page.open()
+            page.click_get_single_resource()
+            status_code = page.check_status_code()
+            body = page.check_result()
+            assert status_code == 200, "Статус кода не соответсвует ожидаемому"
+            assert body['data']['name'] == "fuchsia rose", "Име в теле ответа не соответсвувеет"
