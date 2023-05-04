28031f344683e577b161ea28bea1b5caa60490b5
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-05-04 11:05:20 +0300

    add click get single resource

--- WEB/pages/main_page.py
+++ WEB/pages/main_page.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from WEB.pages.base_page import BasePage
 from WEB.locators.main_page_locators import MainPageLocators
@@ -20,6 +21,12 @@ class Requests(BasePage):
         status_code = self.element_is_present(self.locators.RESPONSE_CODE).text
         return int(status_code)
 
+    def check_status_code_bud(self):
+        """Метод находит и возвращает статус код указанный на главной странице"""
+
+        status_code = self.element_is_present(self.locators.RESPONSE_CODE_BUD).text
+        return int(status_code)
+
     def check_result(self):
         """Метод находит и возвращает тело ответа взятое с главной страницы сайта"""
 
@@ -32,4 +39,25 @@ class Requests(BasePage):
         button = self.element_is_visible(self.locators.GET_SINGLE_USER)
         self.go_to_element(button)
         button.click()
+
+    def click_get_single_user_not_found(self):
+        """Метод кликает на кнопку Get single user not found"""
+
+        button = self.element_is_visible(self.locators.GET_SINGLE_USER_NOT_FOUND)
+        self.go_to_element(button)
+        button.click()
+
+    def click_get_list_resource(self):
+        """Метод кликает на кнопку Get list resource"""
+        button = self.element_is_visible(self.locators.GET_LIST_RESOURCE)
+        self.go_to_element(button)
+        button.click()
+
+    def click_get_single_resource(self):
+        """Метод кликает на кнопку Get single resource"""
+
+        button = self.element_is_visible(self.locators.GET_SINGLE_RESOURCE)
+        self.go_to_element(button)
+        button.click()
+
 
